79666d2e0900b8c0e81c0f0a2f503bf6e775397f
Kadir Engin <kadirengin624@hotmail.com>
2022-12-11 02:06:20 +0300

    add getText api.

--- go.mod
+++ go.mod
@@ -2,9 +2,22 @@ module github.com/Slaykha/OCR
 
 go 1.17
 
-require github.com/otiai10/gosseract v2.2.1+incompatible
+require (
+	github.com/gofiber/fiber v1.14.6
+	github.com/otiai10/gosseract v2.2.1+incompatible
+)
 
 require (
+	github.com/andybalholm/brotli v1.0.0 // indirect
+	github.com/gofiber/utils v0.0.10 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/klauspost/compress v1.10.7 // indirect
+	github.com/mattn/go-colorable v0.1.7 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/otiai10/mint v1.3.3 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.16.0 // indirect
+	github.com/valyala/tcplisten v0.0.0-20161114210144-ceec8f93295a // indirect
 	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,20 +1,89 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
 
+	"github.com/gofiber/fiber"
 	"github.com/otiai10/gosseract"
 )
 
-func main() {
+type Response struct {
+	Text           string `json:"text"`
+	TranslatedText string `json:"translatedText"`
+}
+
+/* func handleTranslate(text string) (*Response, error){
+	translateClient := translate.NewClient("APP_ID", "API_KEY", "SECRET_KEY")
+
+    // Set the text to be translated and the target language
+    text := "Hello, world!"
+    target := "fr"
+
+    // Call the API to translate the text
+    result, err := client.Translate(text, target, "auto")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+} */
+
+func handleGet(c *fiber.Ctx) {
+	// Get the file data from the form-data
+	file, err := c.FormFile("file")
+	if err != nil {
+		c.JSON(err)
+		c.Status(fiber.StatusInternalServerError)
+	}
+
+	// Read the file data
+	fileData, err := file.Open()
+	if err != nil {
+		c.JSON(err)
+		c.Status(fiber.StatusInternalServerError)
+	}
+
+	defer fileData.Close()
+	data, err := ioutil.ReadAll(fileData)
+	if err != nil {
+		c.JSON(err)
+		c.Status(fiber.StatusInternalServerError)
+	}
+
+	// Use gosseract to extract text from the file data
 	client := gosseract.NewClient()
 	defer client.Close()
-	client.SetImage("images.png")
+	client.SetImageFromBytes(data)
 	text, err := client.Text()
 	if err != nil {
-		fmt.Println("Error")
+		c.JSON(err)
+		c.Status(fiber.StatusInternalServerError)
 	}
-	fmt.Println(text)
 
-	// Hello, World!
+	// Create the response data
+	response := Response{
+		Text: text,
+	}
+
+	// Write the response data to the response writer
+	if err := c.JSON(response); err != nil {
+		c.JSON(err)
+		c.Status(fiber.StatusInternalServerError)
+	}
+
+	switch err {
+	case nil:
+		c.JSON(response)
+		c.Status(fiber.StatusCreated)
+	default:
+		c.Status(fiber.StatusInternalServerError)
+	}
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/getText", handleGet)
+
+	app.Listen(8080)
 }
